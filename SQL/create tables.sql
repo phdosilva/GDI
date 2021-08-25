19f976f3900273113257a5b57515c378cc377ee9
Pedro H Silva <33045260+phdosilva@users.noreply.github.com>
2021-08-25 17:59:39 -0300

    ajustes em create e primeriso comandos para popular a tabela

--- SQL/create tables.sql
+++ SQL/create tables.sql
@@ -1,96 +1,97 @@
 CREATE TABLE Profissional (
-    cpf INT,
-    email VARCHAR(50),
-    endereco_rua VARCHAR(255),
-    endereco_numero INT,
-    endereco_bairro VARCHAR(255),
-    endereco_complemento VARCHAR(255),
-    endereco_cidade VARCHAR(255),
-    endereco_estado VARCHAR(255),
+    cpf varchar(11),
+    email varchar(50) NOT NULL,
+    nome varchar(255) NOT NULL,
+    endereco_rua varchar(255),
+    endereco_numero int,
+    endereco_bairro varchar(255),
+    endereco_complemento varchar(255),
+    endereco_cidade varchar(255),
+    endereco_estado varchar(255),
     CONSTRAINT PK_Profissional PRIMARY KEY (cpf)
     );
     
 CREATE TABLE Numero_Contato_Profissional (
-    cpf INT,
-    numero_contato INT,
+    cpf varchar(11),
+    numero_contato varchar(11),
     CONSTRAINT PK_Numero_Contato PRIMARY KEY (cpf, numero_contato),
     CONSTRAINT FK_Numero_Contato_Profissional FOREIGN KEY (cpf) REFERENCES Profissional ON DELETE CASCADE
     );
     
     
 CREATE TABLE Empregador (
-    cpf INT,
+    cpf varchar(11),
     CONSTRAINT PK_Empregador PRIMARY KEY (cpf),
     CONSTRAINT FK_Empregador_Profissional FOREIGN KEY (cpf) REFERENCES Profissional ON DELETE CASCADE
     );
 
 CREATE TABLE Candidato (
-    cpf INT,
-    interesse_salaciral NUMBER,
-    disponibilidade_horarios INT,
-    cargo_almejado VARCHAR(255),
+    cpf varchar(11),
+    interesse_salaciral number(15,2),
+    disponibilidade_horarios int, -- 0=nenhum, 1=manha, 2=tarde, 3=manha e tarde
+    cargo_almejado varchar(255),
     CONSTRAINT PK_Candidato PRIMARY KEY (cpf),
     CONSTRAINT FK_Candidato_Profissional FOREIGN KEY (cpf) REFERENCES Profissional ON DELETE CASCADE
     );
     
 CREATE TABLE Empresa (
-    cnpj INT,
-    nome VARCHAR(255),
-    descricao VARCHAR(255),
-    endereco_rua VARCHAR(255),
-    endereco_numero INT,
-    endereco_bairro VARCHAR(255),
-    endereco_complemento VARCHAR(255),
-    endereco_cidade VARCHAR(255),
-    endereco_estado VARCHAR(255),
-    dirigida_por INT NOT NULL,
+    cnpj varchar(14),
+    nome varchar(255) NOT NULL,
+    descricao varchar(255),
+    endereco_rua varchar(255) NOT NULL,
+    endereco_numero int NOT NULL,
+    endereco_bairro varchar(255) NOT NULL,
+    endereco_complemento varchar(255),
+    endereco_cidade varchar(255) NOT NULL,
+    endereco_estado varchar(255) NOT NULL,
+    dirigida_por varchar(11) NOT NULL,
     CONSTRAINT PK_Empresa PRIMARY KEY (cnpj),
     FOREIGN KEY (dirigida_por) REFERENCES Profissional
     );
     
 CREATE TABLE Conquista (
-    empresa INT,
-    numero_de_registro INT,
-    imagem VARCHAR(255) NOT NULL,
-    titulo VARCHAR(255) NOT NULL,
-    descricao VARCHAR(255) NOT NULL,
-    data_conquista DATE NOT NULL,
+    empresa varchar(11),
+    numero_de_registro varchar(4),
+    imagem varchar(255),
+    titulo varchar(255) NOT NULL,
+    descricao varchar(255),
+    data_conquista date NOT NULL,
     CONSTRAINT PK_Conquista PRIMARY KEY (empresa, numero_de_registro),
     CONSTRAINT FK_Conquista_Empresa FOREIGN KEY (empresa) REFERENCES Empresa ON DELETE CASCADE
     );
     
 CREATE TABLE Vaga (
-    empregador INT,
-    empresa INT,
+    empregador varchar(11),
+    empresa varchar(11),
     CONSTRAINT PK_Vaga PRIMARY KEY (empregador, empresa),
     CONSTRAINT FK_Vaga_Empregador FOREIGN KEY (empregador) REFERENCES Empregador ON DELETE CASCADE,
     CONSTRAINT FK_Vaga_Empresa FOREIGN KEY (empresa) REFERENCES Empresa ON DELETE CASCADE
     );
     
 CREATE TABLE Conectase (
-    profissional_um INT,
-    profissional_dois INT,
+    profissional_um varchar(11),
+    profissional_dois varchar(11),
     CONSTRAINT PK_Conectase PRIMARY KEY (profissional_um, profissional_dois),
     CONSTRAINT FK_Conectase_Profissional_Um FOREIGN KEY (profissional_um) REFERENCES Profissional ON DELETE CASCADE,
     CONSTRAINT FK_Conectase_Profissional_Dois FOREIGN KEY (profissional_dois) REFERENCES Profissional ON DELETE CASCADE
     );
     
 CREATE TABLE Trabalha (
-    profissional INT,
-    empresa INT,
-    data_de_entrada DATE,
-    data_de_saida DATE,
-    cargo VARCHAR(255),
-    descricao VARCHAR(255),
+    profissional varchar(11),
+    empresa varchar(11),
+    data_de_entrada date,
+    data_de_saida date,
+    cargo varchar(255) NOT NULL,
+    descricao varchar(255),
     CONSTRAINT PK_Trabalha PRIMARY KEY (profissional, empresa),
     CONSTRAINT FK_Trabalha_Profissional FOREIGN KEY (profissional) REFERENCES Profissional ON DELETE CASCADE,
     CONSTRAINT FK_Trabalha_Empresa FOREIGN KEY (empresa) REFERENCES Empresa ON DELETE CASCADE
     );
     
 CREATE TABLE Aplica (
-    empregador INT,
-    empresa INT,
-    candidato INT,
+    empregador varchar(11),
+    empresa varchar(11),
+    candidato varchar(11),
     CONSTRAINT PK_Aplica PRIMARY KEY (empregador, empresa, candidato),
     CONSTRAINT FK_Aplica_Vaga FOREIGN KEY (empregador, empresa) REFERENCES Vaga(empregador, empresa) ON DELETE CASCADE,
     CONSTRAINT FK_Aplica_Candidato FOREIGN KEY (candidato) REFERENCES Candidato ON DELETE CASCADE
---
+++ SQL/populate table.sql
@@ -0,0 +1,61 @@
+INSERT INTO Profissional
+VALUES ('0000', 'jpsr@cin.ufpe.br','João Pedro Silva da Rocha', 'rua de joao', 111, 'bairro de joao', 'apt 101', 'cidade de joao', 'estado de joao');
+INSERT INTO Profissional
+VALUES ('1111', 'lrsa@cin.ufpe.br','Layse Roberta da Silva Araújo', 'rua de layse', 111, 'bairro de layse', 'apt 101', 'cidade de layse', 'estado de layse');
+INSERT INTO Profissional
+VALUES ('2222', 'phos@cin.ufpe.br','Pedro Henrique de Oliveira Silva', 'rua de pedro henrique', 111, 'bairro de pedro henrique', 'apt 101', 'cidade de pedro henrique', 'estado de pedro henrique');
+INSERT INTO Profissional
+VALUES ('3333', 'vat2@cin.ufpe.br','Vítor Aguiar Tavares', 'rua de vitor', 111, 'bairro de vitor', 'apt 101', 'cidade de vitor', 'estado de vitor');
+
+INSERT INTO Numero_Contato_Profissional
+VALUES ('0000', '99999999999');
+INSERT INTO Numero_Contato_Profissional
+VALUES ('0000', '88888888888');
+INSERT INTO Numero_Contato_Profissional
+VALUES ('1111', '99998888888');
+INSERT INTO Numero_Contato_Profissional
+VALUES ('2222', '99998888777');
+INSERT INTO Numero_Contato_Profissional
+VALUES ('2222', '99998888770');
+INSERT INTO Numero_Contato_Profissional
+VALUES ('2222', '00098888770');
+
+INSERT INTO Empregador
+VALUES ('0000');
+
+INSERT INTO Candidato
+VALUES ('1111', 1000.50, 1, 'Desenvolvedor');
+INSERT INTO Candidato
+VALUES ('3333', 1000.50, 1, 'Desenvolvedor');
+
+INSERT INTO Empresa
+VALUES ('0000', 'Empresa 1', 'Serviços de desenvolvimento', 'rua da empresa 1', 134, 'bairro da empresa 1', 'complemento da empresa 1', 'cidade da empresa 1', 'estado da empresa 1', '2222')
+INSERT INTO Empresa
+VALUES ('1111', 'Empresa 2', 'Serviços de desenvolvimento', 'rua da empresa 2', 134, 'bairro da empresa 2', 'complemento da empresa 2', 'cidade da empresa 2', 'estado da empresa 2', '2222')
+
+INSERT INTO Conquista
+VALUES ('0000',  '11', 'https://linkdaimagem.com/imagem', 'Bom ambiente de trabalho', 'Aqui é um bom ambiente para se trabalhar', '1000-01-01');
+INSERT INTO Conquista
+VALUES ('1111',  '11', 'https://linkdaimagem.com/imagem', 'Bom ambiente de trabalho', 'Aqui é um bom ambiente para se trabalhar', '1000-01-01');
+
+INSERT INTO Vaga
+VALUES ('0000', '0000');
+
+INSERT INTO Conectase
+VALUES ('0000', '1111');
+INSERT INTO Conectase
+VALUES ('0000', '2222');
+INSERT INTO Conectase
+VALUES ('0000', '3333');
+INSERT INTO Conectase
+VALUES ('1111', '2222');
+INSERT INTO Conectase
+VALUES ('1111', '3333');
+
+INSERT INTO Trabalha
+VALUES ('0000', '0000', '1000-01-01', '9999-12-31', 'Dev Hunter', 'Contrata Desenvolvedores');
+
+INSERT INTO Aplica
+VALUES ('0000', '0000', '1111');
+INSERT INTO Aplica
+VALUES ('0000', '0000', '2222');
